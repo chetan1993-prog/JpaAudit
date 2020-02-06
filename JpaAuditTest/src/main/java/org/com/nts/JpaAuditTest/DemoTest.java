bc3443ea0e8685c102430ed42d2b57adaed71b89
chetan1993-prog <c.kings999@gmail.com>
2020-02-06 14:43:54 +0530

    supplies

--- JpaAuditTest/src/main/java/org/com/nts/JpaAuditTest/DemoTest.java
+++ JpaAuditTest/src/main/java/org/com/nts/JpaAuditTest/DemoTest.java
@@ -6,6 +6,7 @@ public class DemoTest {
 		// TODO Auto-generated method stub
 		System.out.println("my first jenkins job");
 		System.out.println("initial commit");
+		System.out.println("supplies");
 
 	}
 
