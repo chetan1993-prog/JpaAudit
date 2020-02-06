298252258dd23cdc183b25969cd9e86d2df0cd31
chetan1993-prog <c.kings999@gmail.com>
2020-02-06 12:49:24 +0530

    modify DemoTest

--- JpaAuditTest/src/main/java/org/com/nts/JpaAuditTest/DemoTest.java
+++ JpaAuditTest/src/main/java/org/com/nts/JpaAuditTest/DemoTest.java
@@ -5,6 +5,7 @@ public class DemoTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("my first jenkins job");
+		System.out.println("initial commit");
 
 	}
 
